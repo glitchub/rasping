1f8f7d21539a9450f591d54b82a94c5b158c01f0
glitchub <glitchub@users.noreply.github.com>
2020-02-26 14:02:12 -0800

    wip

--- netlink.py
+++ netlink.py
@@ -1,5 +1,5 @@
 from __future__ import print_function
-import os, struct, socket, time, select, fnmatch
+import os, sys, struct, socket, time, select, fnmatch
 # receive network interface status via netlink
 
 # python2 doesn't support monotonic time, use the wall clock for timeouts
@@ -12,8 +12,8 @@ def matches(s, globs):
             return True
     return False
 
-# returned by netlink.dump or netlink.wait, this just a container for the interface data
-class event():
+# returned for link events
+class linkevent():
     def __init__(self, nlmsg, ifi, iff, ifla):
         self.nlmsg=nlmsg
         self.ifi=ifi
@@ -22,9 +22,36 @@ class event():
 
         # create some shortcut accessors
         self.ifname = ifla["ifname"]
-        self.attached = nlmsg["type"]==16 # RTM_NEWLINK
+        self.exists = nlmsg["type"]==16 # RTM_NEWLINK
         self.up = iff["up"]
         self.carrier = None if "carrier" not in ifla else bool(ifla["carrier"])
+
+    def show(self, verbose=False):
+        print("%s: exists=%s up=%s carrier=%s" % (self.ifname, self.exists, self.up, self.carrier))
+        if verbose:
+            print("    nlmsg     =", str(self.nlmsg))
+            print("    ifi       =", str(self.ifi))
+            print("    iff       =", str(self.iff))
+            print("    ifla      =", str(self.ifla))
+
+# returned for address events
+class addrevent():
+    def __init__(self, nlmsg, ifaddrmsg, ifa):
+        self.nlmsg = nlmsg
+        self.ifaddrmsg = ifaddrmsg
+        self.ifa = ifa
+
+        self.ifname = self.label = ifa["label"]
+        self.exists = nlmsg["type"] == 20 # RTM_NEWADDR
+        self.prefix = self.prefixlen = ifaddrmsg["prefixlen"]
+        self.address = ifa.get("address")
+
+    def show(self, verbose=False):
+        print("%s: exists=%s address=%s prefix=%d" % (self.ifname, self.exists, self.address, self.prefix))
+        if verbose:
+            print("    nlmsg     =", str(self.nlmsg))
+            print("    ifaddrmsg =", str(self.ifaddrmsg))
+            print("    ifa       =", str(self.ifa))
 
 class netlink():
     # nlmsg_xxx symbols from netlink.h, minus the "nlmsg_"
@@ -34,7 +61,7 @@ class netlink():
     _ifi_fields = [ "family", "type", "index", "flags", "changes" ]
 
     # Lowercase versions of IFLA_XXX symbols from if_link.h, minus the "IFLA_"
-    _ifla_fields = [ "none", "address", "broadcast", "ifname", "mtu", "link",
+    _ifla_fields = [ "unspec", "address", "broadcast", "ifname", "mtu", "link",
                      "qdisc", "stats", "cost", "priority", "master",
                      "wireless", "protinfo", "txqlen", "map", "weight",
                      "operstate", "linkmode", "linkinfo", "net_ns_pid",
@@ -61,66 +88,118 @@ class netlink():
                   "master", "slave", "multicast", "portsel", "automedia",
                   "dynamic", "lower_up", "dormant", "echo" ]
 
-    # Open the netlink socket, debug flag spews stuff to stdout
-    # If dump is set, request status for all existing interfaces
-    def __init__(self, debug=False):
-        self.debug=debug
-        self.sock=socket.socket(socket.AF_NETLINK, socket.SOCK_RAW, socket.NETLINK_ROUTE)
-        self.sock.bind((os.getpid(),1)) # RTMGRP_LINK messages
+    # Fields in struct ifaddrmsg, minus the "ifa_"
+    _ifaddrmsg_fields = [ "family", "prefixlen", "flags", "scope", "index" ]
 
-    # process data as netlink attributes and return ifla dict, or None
-    def _attributes(self, data):
+    # Lowercase versions if IFA_XXX symbols from if_link.h, minus the "IFA_"
+    _ifa_fields = [ "unspec", "address", "local", "label", "broadcast",
+                    "anycast", "cacheinfo", "multicast", "flags" ]
+
+    # Open the netlink socket, debug flag spews stuff to stdout.
+    # Mode is "links", "addresses", or "both". Default is "links". Only the
+    # first character actually matters.
+    def __init__(self, mode="links", debug=0):
+        self.debug=debug
+        self.mode=mode[0]
+        if self.mode not in ["l","a","b"]: raise Exception('Must specify mode="l", mode="a", or mode="b"')
+
+        self.sock=socket.socket(socket.AF_NETLINK, socket.SOCK_RAW, socket.NETLINK_ROUTE)
+        self.sock.bind((os.getpid(), (0 if self.mode[0] == "a" else 1) | (0 if self.mode[0] == "l" else 0x10))) # RTMGRP_LINK, RTMGRP_IPV4_IFADDRS
+
+    def _debug(self, *args, level=1):
+        if self.debug >= level: print(args, file=sys.stderr )
+
+    # process ifla attribute and return { field, data }, or None
+    def _parse_ifla(self, nla_type, data):
+        if nla_type >= len(self._ifla_fields):
+            self._debug("Invalid IFLA nla_type", type)
+            return None
+
+        field = self._ifla_fields[nla_type]
+
+        if field in ["address", "broadcast"]:
+            # colon-separated hex octets
+            return { field : ":".join(list("%02X" % c for c in list(bytearray(data)))) }
+
+        if field in ["ifname", "ifalias", "qdisc"]:
+            # NUL-terminated string
+            return { field : data.split(b"\x00")[0].decode("ascii") }
+
+        if field == "stats64":
+            # struct rtnl_link_stats64, 24 unsigned long longs (64-bit), call it "stats"
+            return { "stats": dict(zip(self._stat_fields, struct.unpack("=24Q", data))) }
+
+        if field in ["phy_port_id", "phys_switch_id"]:
+            # raw hex
+            return { field : data.hex() }
+
+        if len(data) == 1:
+            # generic 8-bit data
+            return { field : struct.unpack("B", data)[0] }
+
+        if len(data) == 4:
+            # generic 32-bit data
+            return { field : struct.unpack("=L", data)[0] }
+
+        self._debug("Don't know how to decode IFLA_%s" % field.upper())
+
+        return None
+
+    # process ifa_attribute and { field: value } or None
+    def _parse_ifa(self, nla_type, data):
+        if nla_type >= len(self._ifa_fields):
+            self._debug("Invalid IFA nla_type", type)
+            return None
+
+        field = self._ifa_fields[nla_type]
+
+        if field in ["address", "local", "broadcast", "anycast", "multicast" ]:
+            # dot-separated decimal octets
+            return { field : ".".join(list("%d" % c for c in list(bytearray(data)))) }
+
+        if field == "label":
+            # NUL-terminated string
+            return { field : data.split(b"\x00")[0].decode("ascii") }
+
+        if len(data) == 1:
+            # generic 8-bit data
+            return { field : struct.unpack("B", data)[0] }
+
+        if len(data) == 4:
+            # generic 32-bit data
+            return { field : struct.unpack("=L", data)[0] }
+
+        self._debug("Don't know how to decode IFA_%s" % field.upper())
+        return None
+
+    # process attribute data with specified parser and return dict
+    def _attributes(self, data, parser):
         if len(data) <= 4: return None
-        ifla={}
+        attrs={}
         while len(data) >= 4:
+
             # first 4 bytes is struct nlattr (netlink.h) then attribute data depending on nla_len
             nla_len, nla_type = struct.unpack("=HH", data[:4])
-            if self.debug: print("nla_len=%d, nla_type=%d" % (nla_len, nla_type))
+            self._debug("nla_len=%d, nla_type=%d" % (nla_len, nla_type), level=2)
             if (nla_len < 4): break # done!
             if nla_len > len(data):
-                if self.debug: print("Only %d bytes of remaining data!" % len(data))
+                self._debug("Only %d bytes of remaining data!" % len(data))
                 return None
 
-            if nla_type < len(self._ifla_fields):
-                field = self._ifla_fields[nla_type]
-
-                if field in ["address", "broadcast"]:
-                    # colon-separated octets
-                    ifla[field] = ":".join(list("%02X" % c for c in list(bytearray(data[4:nla_len]))))
-                elif field in ["ifname", "ifalias"]:
-                    # NUL-terminated string
-                    ifla[field] = data[4:nla_len].split(b"\x00")[0].decode("ascii")
-                elif field == "stats":
-                    # struct rtnl_link_stats, 24 unsigned ints
-                    # don't overwrite existing "stats"
-                    if not ifla.get("stats"): ifla["stats"]=dict(zip(self._stat_fields, struct.unpack("=24L", data[4:nla_len])))
-                elif field == "stats64":
-                    # struct rtnl_link_stats64, same fields fields as stats but 24 unsigned long longs (64-bit),
-                    # always overwrite existing "stats"
-                    ifla["stats"] = dict(zip(self._stat_fields, struct.unpack("=24Q", data[4:nla_len])))
-                elif field in ["phy_port_id", "phys_switch_id"]:
-                    # raw hex
-                    ifla[field] = data[4:nla_len].hex()
-                elif nla_len == 5:
-                    # generic 8-bit data
-                    ifla[field]=struct.unpack("B", data[4:5])[0]
-                elif nla_len == 8:
-                    # generic 32-bit data
-                    ifla[field]=struct.unpack("=L", data[4:8])[0]
-                elif self.debug:
-                    print("Don't know how to decode IFLA_%s" % field.upper())
+            attr = parser(nla_type, data[4:nla_len])
+            if attr: attrs.update(attr)
 
             # skip to next attribute, note we pad nla_len to multiple of 4
             data = data[((nla_len+3) & ~3):]
+        return attrs
 
-        return ifla
 
     # Wait for a netlink event.  Note this is an iterator, use with "for".  If
     # timeout (seconds) is specified, exits if no event reported within that
     # time (raises StopIteration). If strict is set, do not return events that
-    # don't specify carrier state. Accept and reject are lists of globs, only
-    # interfaces that match an accept glob and do not match a reject glob will
-    # be returned, by default returns everything.
+    # don't specify carrier state or ip address. Accept and reject are lists of
+    # globs, only interfaces that match an accept glob and do not match a
+    # reject glob will be returned, by default returns everything.
     def wait(self, timeout=None, strict=True, accept=['*'], reject=[]):
         expire = 0
         while True:
@@ -129,73 +208,93 @@ class netlink():
                 s = select.select([self.sock],[],[], max(0, expire-time.monotonic()))
                 if not s[0]: return
             data = self.sock.recv(65535)
-            if self.debug: print("Netlink packet is %d bytes" % len(data))
+            self._debug("Netlink packet is %d bytes" % len(data))
 
             while len(data) >= 16:
                 # first 16 bytes is struct nlmsghdr (netlink.h)
                 nlmsg = dict(zip(self._nlmsg_fields, struct.unpack("=LHHLL", data[0:16])))
-                if self.debug: print("nlmsg =", nlmsg)
+                self._debug("nlmsg =", nlmsg)
                 if nlmsg["len"] > len(data): break
 
                 if nlmsg["type"] == 3: return  # NLMSG_DONE, occurs at the end of a dump (netlink.h)
 
+
                 if len(data) >= 32 and nlmsg["type"] in [16,17]:  # RTM_NEWLINK or RTM_DELLINK (rtnetlink.h)
                     # next 16 bytes is ifinfomsg (rtnetlink.h)
                     ifi = dict(zip(self._ifi_fields, struct.unpack("=BxHlLL",data[16:32])))
-                    if self.debug: print("ifi =", ifi)
+                    self._debug("ifi =", ifi)
                     # parse interface flags to iff
                     iff={}
                     for b,v in enumerate(self._iff_bits): iff[v]=bool(ifi["flags"] & (1 << b))
-                    if self.debug: print("iff =", iff)
+                    self._debug("iff =", iff)
 
                     # process the rest of the packet for netlink attributes
-                    ifla = self._attributes(data[32 : nlmsg["len"]])
+                    ifla = self._attributes(data[32 : nlmsg["len"]], self._parse_ifla)
                     if ifla:
-                        if self.debug: print("ifla =", ifla)
+                        self._debug("ifla =", ifla)
                         ifname=ifla.get("ifname")
                         if not ifname:
-                            if self.debug: print("Dropping event without ifname")
+                            self._debug("Dropping linkevent without ifname")
                         elif strict and "carrier" not in ifla:
-                            if self.debug: print("Dropping event without carrier")
+                            self._debug("Dropping linkevent without carrier")
                         elif matches(ifname, reject) or not matches(ifname, accept):
-                            if self.debug: print("Dropping event from", ifname)
+                            self._debug("Dropping linkevent from", ifname)
                         else:
-                            yield event(nlmsg, ifi, iff, ifla)  # yield an event
-                            expire = 0                          # restart the timeout
+                            yield linkevent(nlmsg, ifi, iff, ifla)  # yield linkevent
+                            expire = 0                              # restart the timeout
+
+                elif len(data) >= 24 and nlmsg["type"] in [20, 21]: # RTM_NEWADDR or RTM_DELADDR (rtnetlink.h)
+                    # first 8 bytes if struct ifaddrmsg
+                    ifaddrmsg = dict(zip(self._ifaddrmsg_fields, struct.unpack("=BBBBL", data[16:24])))
+                    self._debug("ifaddrmsg =", ifaddrmsg)
+                    ifa = self._attributes(data[24: nlmsg["len"]], self._parse_ifa)
+                    if ifa:
+                        self._debug("ifa =", ifa)
+                        ifname=ifa.get("label")
+                        if not ifname:
+                            self._debug("Dropping addrevent without ifname")
+                        elif strict and "address" not in ifa:
+                            self._debug("Dropping addrevent without address")
+                        elif matches(ifname, reject) or not matches(ifname, accept):
+                            self._debug("Dropping addrevent from", ifname)
+                        else:
+                            yield addrevent(nlmsg, ifaddrmsg, ifa)  # yield addrevent
+                            expire = 0                              # restart the timeout
 
                 # advance to next packet, if any
                 data = data[nlmsg["len"]:]
 
     # Issue netlink dump command and yield all matching interfaces, then exit
-    def dump(self, accept=['*'], reject=[]):
+    # Mode is ignored unless the netlink object was created with mode="both", then it
+    # must "links" or "addrs", default is "links"
+    def dump(self, mode="links", accept=['*'], reject=[]):
+        if self.mode[0] != "b": mode=self.mode
+        if mode[0] not in ["a", "l"]: raise Exception('Must specify mode="l" or mode="a"')
+
         data = struct.pack("=LHHLLL",
-            20,             # nlmsg_len = total length of packet
-            18,             # nlmsg_type = RTM_GETLINK
-            0x0301,         # nlmsg_flags = NLM_F_ROOT|NLM_F_MATCH|NLM_F_REQUEST
-            1,              # nlmsg_seq
-            os.getpid(),    # nlmsg_pid
-            17              # rtgen_familiy = AF_PACKET
+            20,                             # nlmsg_len = total length of packet
+            22 if mode[0]=="a" else 18,     # nlmsg_type = RTM_GETLINK or RTM GETADDR
+            0x0301,                         # nlmsg_flags = NLM_F_ROOT|NLM_F_MATCH|NLM_F_REQUEST
+            1,                              # nlmsg_seq
+            os.getpid(),                    # nlmsg_pid
+            17                              # rtgen_familiy = AF_PACKET
         )
         self.sock.send(data)
         return self.wait(timeout=1, accept=accept, reject=reject)
 
 if __name__ == "__main__":
-    nl=netlink()   # create the netlink object
+    nl=netlink(mode="both")  # create the netlink object
 
     print("Current interfaces:\n")
-    for e in nl.dump():
-        print("    %s: attached=%s up=%s carrier=%s" % (e.ifname, e.attached, e.up, e.carrier))
+    for e in nl.dump(mode="links"): e.show(verbose=True)
+    for e in nl.dump(mode="addrs"): e.show(verbose=True)
 
     print("\nWaiting for events...\n")
     while True:
         # Return interface events except from lo, timeout after 5 seconds
         active=False
         for e in nl.wait(timeout=5, reject=['lo']):
-            print("nlmsg =", str(e.nlmsg))
-            print("ifi   =", str(e.ifi))
-            print("iff   =", str(e.iff))
-            print("ifla  =", str(e.ifla))
-            print("%s: attached=%s up=%s carrier=%s\n" % (e.ifname, e.attached, e.up, e.carrier))
+            e.show(verbose=True)
             active=True
         # timeout
         if active:
